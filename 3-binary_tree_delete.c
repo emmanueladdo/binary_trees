71f6316bddc21d3a354b034855c10bd65d90f20f
root <root@DESKTOP-KUT53MR>
2023-08-01 15:26:42 +0000

    delete tree

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,16 @@
+#include "binary_tree.h"
+
+/**
+ * binary_tree_delete -delete - deletes a binary tree
+ * @tree: represnts the tree
+ */
+
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree != NULL)
+	{
+		binary_tree_delete(tree->right);
+		binary_tree_delete(tree->left);
+		free(tree);
+	}
+}
