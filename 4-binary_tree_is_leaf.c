822ae360c84be37b8ebac90f760cca0ba63a3f45
root <root@DESKTOP-KUT53MR>
2023-08-01 15:57:26 +0000

    check for left

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_leaf - check if node is a leaf 
+ * @node: the node to check
+ * Return: retruns 1 if true or 0 if false
+ */
+
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node != NULL)
+		if (node->left == NULL && node->right == NULL)
+			return (1);
+		else
+			return (0);
+	else
+		return (0);
+}
