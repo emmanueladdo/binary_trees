ca7c6e44cbbc1ce2be33921612db181ed99c4045
root <root@DESKTOP-KUT53MR>
2023-08-06 06:32:15 +0000

    is binary search tree

---
+++ 110-binary_tree_is_bst.c
@@ -0,0 +1,48 @@
+/**
+ * bst_lesser - BST validator function lesser helper
+ * @node: node to compare
+ * @val: value to compare with
+ * Return: 1 if valid BST, else 0
+ */
+
+int bst_lesser(const binary_tree_t *node, int val)
+{
+	if (node == NULL)
+		return (1);
+	if (node->n >= val)
+		return (0);
+
+	return (bst_lesser(node->left, val) &&
+		bst_lesser(node->right, val));
+}
+
+/**
+ * bst_greater - BST validator function greater helper
+ * @node: node to compare
+ * @val: value to compare with
+ * Return: 1 if valid BST, else 0
+ */
+int bst_greater(const binary_tree_t *node, int val)
+{
+	if (node == NULL)
+		return (1);
+	if (node->n <= val)
+		return (0);
+
+	return (bst_greater(node->left, val) &&
+		bst_greater(node->right, val));
+}
+
+/**
+ * binary_tree_is_bst - Check if a binary tree is a valid binary search tree
+ * @tree: pointer to root node of tree to check
+ * Return: 1 if valid BST, else 0
+ */
+int binary_tree_is_bst(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	return (bst_lesser(tree->left, tree->n) &&
+		bst_greater(tree->right, tree->n));
+}
