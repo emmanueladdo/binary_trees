2ce2c0a06ad3ba4d4752e93baa6753b3b5aac121
root <root@DESKTOP-KUT53MR>
2023-08-06 05:43:50 +0000

    traverse using levelorder

--- binary_trees.h
+++ binary_trees.h
@@ -28,6 +28,7 @@ typedef struct binary_tree_s avl_t;
 /*helper functtion*/
 void binary_tree_print(const binary_tree_t *);
 int new_binary_tree_height(const binary_tree_t *tree);
+void levelorderhelp(const binary_tree_t *tree, size_t level, void (*func)(int));
 
 /* Mandatory task*/
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
@@ -49,6 +50,7 @@ int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
+void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
 
 /*ADVANCE TASKS*/
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
