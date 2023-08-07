e3aabd42a153c60bb614946b05c9286c56df330e
root <root@DESKTOP-KUT53MR>
2023-08-07 10:38:08 +0000

    removes

--- binary_trees.h
+++ binary_trees.h
@@ -29,6 +29,7 @@ typedef struct binary_tree_s avl_t;
 void binary_tree_print(const binary_tree_t *);
 int new_binary_tree_height(const binary_tree_t *tree);
 void levelorderhelp(const binary_tree_t *tree, size_t level, void (*func)(int));
+bst_t *bst_find_root(bst_t *node);
 
 /* Mandatory task*/
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
